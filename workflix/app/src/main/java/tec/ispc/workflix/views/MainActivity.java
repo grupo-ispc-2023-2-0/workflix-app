0b33b506e9f892b578af8577e57f71907dca2cce
Braian <troncosobraianaxel@gmail.com>
2023-09-19 21:44:34 -0300

    [ADD] esqueleto proyecto

---
+++ workflix/app/src/main/java/tec/ispc/workflix/views/MainActivity.java
@@ -0,0 +1,19 @@
+package tec.ispc.workflix.views;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import tec.ispc.workflix.R;
+import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
