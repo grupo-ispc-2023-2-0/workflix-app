ae00cc9c8467bbafd8c1b05d2e1c6eb00b00b5bb
Braian <troncosobraianaxel@gmail.com>
2023-09-26 00:57:23 -0300

    [ADD] imports android mainActivity

--- workflix/app/src/main/java/tec/ispc/workflix/views/MainActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/MainActivity.java
@@ -7,9 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import tec.ispc.workflix.views.ui.login.LoginActivity;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
