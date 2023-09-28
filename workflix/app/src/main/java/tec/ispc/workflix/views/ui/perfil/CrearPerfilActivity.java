02c8b463bc46a1fe3f98cfedad9eaf7f2b3bc091
Yandira Diaz <yandiradiaz@hotmail.com>
2023-09-28 20:26:12 -0300

    se agrega navegabilidad botones eliminar cuenta

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/perfil/CrearPerfilActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/perfil/CrearPerfilActivity.java
@@ -2,9 +2,12 @@ package tec.ispc.workflix.views.ui.perfil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import tec.ispc.workflix.R;
+import tec.ispc.workflix.views.ui.eliminar_cuenta.EliminarCuentaActivity;
 
 public class CrearPerfilActivity extends AppCompatActivity {
 
@@ -12,5 +15,9 @@ public class CrearPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_perfil);
+    }
+    public void cerrarCuenta (View view) {
+        Intent intent = new Intent(this, EliminarCuentaActivity.class);
+        startActivity(intent);
     }
 }
