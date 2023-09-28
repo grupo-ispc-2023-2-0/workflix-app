50d2ed17a5c49a01efabe9a620503fd175f83462
Braian <troncosobraianaxel@gmail.com>
2023-09-28 02:13:08 -0300

    [ADD] sobre nosotros terminada

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/AboutFragment.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/AboutFragment.java
@@ -1,5 +1,7 @@
 package tec.ispc.workflix.views.ui.menu;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +9,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import tec.ispc.workflix.R;
 
 
 public class AboutFragment extends Fragment {
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView linkTextView = view.findViewById(R.id.linkTextView);
+
+        linkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.google.com.ar";
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
