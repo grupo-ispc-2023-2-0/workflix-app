37c8d185ea8436ecf95972b203b991cd44237091
marceloedelgado <marceloedelgado@gmail.com>
2023-09-28 19:53:29 -0300

    De la activity_catalogo.xml comiteando los cambios de insercion de logo + barra de busqueda + botones de filtro de profesiones

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/HomeFragment.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/HomeFragment.java
@@ -1,5 +1,6 @@
 package tec.ispc.workflix.views.ui.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import tec.ispc.workflix.R;
+import tec.ispc.workflix.views.ui.catalogo.CatalogoActivity;
 
 
 public class HomeFragment extends Fragment {
 
+    private Button btn_home2;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btn_home2 = view.findViewById(R.id.btn_home2);
+
+        btn_home2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), CatalogoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
     }
 }
