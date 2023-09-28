ce2aafd06a829dd37e1e8a8bcf984a3ce86d7750
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-09-28 12:52:14 -0300

    agrega intent a perfil terminos

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/perfil_terminos/PerfilTerminosActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/perfil_terminos/PerfilTerminosActivity.java
@@ -2,9 +2,12 @@ package tec.ispc.workflix.views.ui.perfil_terminos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import tec.ispc.workflix.R;
+import tec.ispc.workflix.views.ui.perfil.CrearPerfilActivity;
 
 public class PerfilTerminosActivity extends AppCompatActivity {
 
@@ -12,5 +15,9 @@ public class PerfilTerminosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_terminos);
+    }
+    public void irPerfilEditar(View view) {
+        Intent irPerfilEditarIntent = new Intent(this, CrearPerfilActivity.class);
+        startActivity(irPerfilEditarIntent);
     }
 }
