51a485d5de751db98c3f598c655a373d9f2839db
Erick <105466809+erick-lpz@users.noreply.github.com>
2023-09-29 00:32:37 -0300

    Agregar función de volver al menu en el dashboard

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/dashboard_admin/DashboardAdminActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/dashboard_admin/DashboardAdminActivity.java
@@ -2,9 +2,13 @@ package tec.ispc.workflix.views.ui.dashboard_admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import tec.ispc.workflix.R;
+import tec.ispc.workflix.views.MainActivity;
+import tec.ispc.workflix.views.ui.register.RegisterActivity;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
@@ -13,6 +17,10 @@ public class DashboardAdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_admin);
 
+    }
 
+    public void irInicio(View view) {
+        Intent inicioIntent = new Intent(this, MainActivity.class);
+        startActivity(inicioIntent);
     }
 }
