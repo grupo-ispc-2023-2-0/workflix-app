fe42e7d7191a16f4af4a44fe81405d6fff356c6a
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2023-09-29 00:52:05 -0300

    agrega clase editar perfil

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/perfil/EditarPerfil.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/perfil/EditarPerfil.java
@@ -1,2 +1,16 @@
-package tec.ispc.workflix.views.ui.perfil;public class EditarPerfil {
+package tec.ispc.workflix.views.ui.perfil;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import tec.ispc.workflix.R;
+
+public class EditarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_editar);
+    }
 }
