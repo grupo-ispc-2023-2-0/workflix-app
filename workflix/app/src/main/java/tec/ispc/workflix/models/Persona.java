f1207b4e2f5dea911946ab0b6d78f5b368eee1fd
Braian <troncosobraianaxel@gmail.com>
2023-10-03 17:26:49 -0300

    [ADD] creando modelos

---
+++ workflix/app/src/main/java/tec/ispc/workflix/models/Persona.java
@@ -0,0 +1,32 @@
+package tec.ispc.workflix.models;
+
+public class Persona {
+    private int id;
+    private String nombres;
+    private String apellidos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+}
