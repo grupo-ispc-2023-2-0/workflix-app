54b6bac890ec856865d66213c883c2dbb3f42761
Braian <troncosobraianaxel@gmail.com>
2023-10-03 18:57:32 -0300

    [ADD] creacion clase persona service

--- workflix/app/src/main/java/tec/ispc/workflix/utils/PersonaService.java
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/PersonaService.java
@@ -1,4 +1,11 @@
 package tec.ispc.workflix.utils;
 
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import tec.ispc.workflix.models.Persona;
+
 public interface PersonaService {
+    Call<List<Persona>> getPersonas();
 }
