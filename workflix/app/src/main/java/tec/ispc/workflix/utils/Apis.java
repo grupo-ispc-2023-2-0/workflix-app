c943c9ac5c354ba64505aa6e610a6de19a9022ad
Braian <troncosobraianaxel@gmail.com>
2023-10-03 19:18:45 -0300

    [ADD] retrofit url - completo

--- workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
@@ -1,4 +1,9 @@
 package tec.ispc.workflix.utils;
 
 public class Apis {
+    public static final String URL_001="http://192.168.1.175:8080/";
+
+    public static PersonaService getPersonaService(){
+        return Cliente.getCliente(URL_001).create(PersonaService.class);
+    }
 }
--- workflix/app/src/main/java/tec/ispc/workflix/utils/Cliente.java
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/Cliente.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Cliente {
-    public Retrofit getCliente(String url){
+    public static Retrofit getCliente(String url){
         Retrofit retrofit = new Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
     return retrofit;
     }
