59825efaf21e4592561b295e24eba4d84834f5b4
Braian <troncosobraianaxel@gmail.com>
2023-10-04 03:14:56 -0300

    [ADD] act ip

--- workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
@@ -1,7 +1,7 @@
 package tec.ispc.workflix.utils;
 
 public class Apis {
-    public static final String URL_001="http://192.168.0.106:8080/personas/";
+    public static final String URL_001="http://127.0.0.1:8080/personas/";
 
     public static PersonaService getPersonaService(){
         return Cliente.getCliente(URL_001).create(PersonaService.class);
