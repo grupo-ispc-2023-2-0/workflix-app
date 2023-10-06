71f70ec52029f021af19f80d6eabe0263e96008d
Braian <troncosobraianaxel@gmail.com>
2023-10-06 03:33:51 -0300

    [ADD] listar ok - usuarios

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/dashboard_admin/ListarUsuariosActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/dashboard_admin/ListarUsuariosActivity.java
@@ -2,6 +2,8 @@ package tec.ispc.workflix.views.ui.dashboard_admin;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -56,5 +58,27 @@ public class ListarUsuariosActivity extends AppCompatActivity {
                 Log.e("Error:",t.getMessage());
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
