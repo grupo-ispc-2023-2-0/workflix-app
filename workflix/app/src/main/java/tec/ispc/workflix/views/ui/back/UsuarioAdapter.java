44afd2b2b86abd66f58c0163c8c24ea031c1a986
Braian <troncosobraianaxel@gmail.com>
2023-10-06 02:14:19 -0300

    [ADD] creacion usuario adapter

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/back/UsuarioAdapter.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/back/UsuarioAdapter.java
@@ -1,4 +1,26 @@
 package tec.ispc.workflix.views.ui.back;
 
-public class UsuarioAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import java.util.List;
+
+import tec.ispc.workflix.R;
+import tec.ispc.workflix.models.Usuario;
+public class UsuarioAdapter extends ArrayAdapter<Usuario>{
+    private Context context;
+    private List<Usuario>usuarios;
+
+    public UsuarioAdapter(@NonNull Context context, int resource, @NonNull List<Usuario> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.usuarios = objects;
+    }
 }
