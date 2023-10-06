f8a525774c5e45ef8cdcc7819701153ec1a2ef87
Braian <troncosobraianaxel@gmail.com>
2023-10-06 00:32:39 -0300

    [ADD] Models - Usuario - android - front

--- workflix/app/src/main/java/tec/ispc/workflix/models/Usuario.java
+++ workflix/app/src/main/java/tec/ispc/workflix/models/Usuario.java
@@ -8,5 +8,82 @@ public class Usuario {
     @Expose
     private int id;
 
+    @SerializedName("nombre")
+    @Expose
+    private String nombre;
 
+    @SerializedName("apellido")
+    @Expose
+    private String apellido;
+
+    @SerializedName("clave")
+    @Expose
+    private String clave;
+
+    @SerializedName("telefono")
+    @Expose
+    private String telefono;
+
+    @SerializedName("correo")
+    @Expose
+    private String correo;
+
+    public Usuario(){};
+
+    public Usuario(int id, String nombre, String apellido, String clave, String telefono, String correo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.clave = clave;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
