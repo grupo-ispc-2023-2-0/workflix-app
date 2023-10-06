62e72532901a863ab178a8f468fdf6245e339e89
Braian <troncosobraianaxel@gmail.com>
2023-10-06 03:21:37 -0300

    [ADD] vinculando usuario adapter a listar activity

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/dashboard_admin/ListarUsuariosActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/dashboard_admin/ListarUsuariosActivity.java
@@ -1,14 +1,21 @@
 package tec.ispc.workflix.views.ui.dashboard_admin;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 import tec.ispc.workflix.R;
+import tec.ispc.workflix.models.Usuario;
 import tec.ispc.workflix.utils.UsuarioService;
 
 public class ListarUsuariosActivity extends AppCompatActivity {
     UsuarioService usuarioService;
+    List<Usuario> listarUsuario= new ArrayList<>();
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
