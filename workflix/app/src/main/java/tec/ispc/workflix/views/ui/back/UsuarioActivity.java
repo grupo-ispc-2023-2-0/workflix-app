00ce53efdcbaae21371509e7c82cbae36bb19837
Braian <troncosobraianaxel@gmail.com>
2023-10-06 04:10:01 -0300

    [ADD] usuario activity - xml - variables

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/back/UsuarioActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/back/UsuarioActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import tec.ispc.workflix.R;
 import tec.ispc.workflix.models.Persona;
 import tec.ispc.workflix.models.Usuario;
+import tec.ispc.workflix.utils.Apis;
 import tec.ispc.workflix.utils.UsuarioService;
 import tec.ispc.workflix.views.FalsoMain;
 import tec.ispc.workflix.views.ui.dashboard_admin.ListarUsuariosActivity;
@@ -105,5 +111,61 @@ public class UsuarioActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void addUsuario(Usuario usuario){
+        usuarioService = Apis.getUsuarioService();
+        Call<Usuario> call=usuarioService.addUsuario(usuario);
+        call.enqueue(new Callback<Usuario>() {
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                if(response.isSuccessful()){
+                    Toast.makeText(UsuarioActivity.this,"Se agrego un Usuario con éxito",Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+                Log.e("Error al agregar un Usuario:",t.getMessage());
+            }
+        });
+        Intent intent =new Intent(UsuarioActivity.this, ListarUsuariosActivity.class);
+        startActivity(intent);
+    }
+    public void updateUsuario(Usuario usuario,int id){
+        service= Apis.getPersonaService();
+        Call<Persona>call=service.updatePersona(p,id);
+        call.enqueue(new Callback<Persona>() {
+            @Override
+            public void onResponse(Call<Persona> call, Response<Persona> response) {
+                if(response.isSuccessful()){
+                    Toast.makeText(PersonaActivity.this,"Se Actualizó con éxito",Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<Persona> call, Throwable t) {
+                Log.e("Error:",t.getMessage());
+            }
+        });
+        Intent intent =new Intent(PersonaActivity.this, FalsoMain.class);
+        startActivity(intent);
+    }
+    public void deletePersona(int id){
+        service=Apis.getPersonaService();
+        Call<Persona>call=service.deletePersona(id);
+        call.enqueue(new Callback<Persona>() {
+            @Override
+            public void onResponse(Call<Persona> call, Response<Persona> response) {
+                if(response.isSuccessful()){
+                    Toast.makeText(PersonaActivity.this,"Se Elimino el registro",Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Persona> call, Throwable t) {
+                Log.e("Error:",t.getMessage());
+            }
+        });
+        Intent intent =new Intent(PersonaActivity.this,FalsoMain.class);
+        startActivity(intent);
     }
 }
