820a84c4f1249fff56b79970579a08344f5a07b4
Braian <troncosobraianaxel@gmail.com>
2023-10-06 03:34:39 -0300

    [ADD] usuario activity

---
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/back/UsuarioActivity.java
@@ -0,0 +1,16 @@
+package tec.ispc.workflix.views.ui.back;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tec.ispc.workflix.R;
+
+public class UsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+    }
+}
