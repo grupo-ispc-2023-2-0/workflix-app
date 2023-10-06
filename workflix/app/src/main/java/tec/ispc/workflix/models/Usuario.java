c718a238c6dd602d2be2f00865f42bd005e34350
Braian <troncosobraianaxel@gmail.com>
2023-10-06 00:30:04 -0300

    [ADD] Models - Usuario - android - front

--- workflix/app/src/main/java/tec/ispc/workflix/models/Usuario.java
+++ workflix/app/src/main/java/tec/ispc/workflix/models/Usuario.java
@@ -1,4 +1,12 @@
 package tec.ispc.workflix.models;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 public class Usuario {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+
 }
