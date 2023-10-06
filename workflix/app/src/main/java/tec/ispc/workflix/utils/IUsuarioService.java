fc4bd0aabf5ef6f31ce4111ff8f8d6a26f59b11f
Braian <troncosobraianaxel@gmail.com>
2023-10-06 00:34:57 -0300

    [ADD] Utils - IUsuarioService - android - front

---
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/IUsuarioService.java
@@ -0,0 +1,11 @@
+package tec.ispc.workflix.utils;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import tec.ispc.workflix.models.Usuario;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+public interface IUsuarioService {
+
+}
