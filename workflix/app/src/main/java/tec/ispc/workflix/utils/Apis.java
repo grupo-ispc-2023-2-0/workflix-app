6c1da50ee3fc9276a5e5ff4c3b540b6fda395d0b
Braian <troncosobraianaxel@gmail.com>
2023-10-08 15:10:21 -0300

    [DEL] persona model

--- workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
@@ -1,13 +1,10 @@
 package tec.ispc.workflix.utils;
 
 public class Apis {
-    public static final String URL_001="http://192.168.0.125:8080/personas/";
+    //public static final String URL_001="http://192.168.0.125:8080/personas/";
     public static final String URL_002="http://192.168.0.125:8080/usuarios/";
 
 
-    public static PersonaService getPersonaService(){
-        return Cliente.getCliente(URL_001).create(PersonaService.class);
-    }
 
     public static UsuarioService getUsuarioService(){
         return Cliente.getCliente(URL_002).create(UsuarioService.class);
