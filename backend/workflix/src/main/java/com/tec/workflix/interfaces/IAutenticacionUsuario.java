a11f38cdbdd79119f3e98fc47d7f5408d8d53935
Braian <troncosobraianaxel@gmail.com>
2023-10-14 02:45:22 -0300

    [ADD] Interfacie autenticacion Usuarios

--- backend/workflix/src/main/java/com/tec/workflix/interfaces/IAutenticacionUsuario.java
+++ backend/workflix/src/main/java/com/tec/workflix/interfaces/IAutenticacionUsuario.java
@@ -1,4 +1,8 @@
 package com.tec.workflix.interfaces;
 
+import com.tec.workflix.models.Usuario;
+
 public interface IAutenticacionUsuario {
+    public Usuario obtenerUsuario(Usuario usuario);
+    public int add(Usuario usuario);
 }
