a490a0a0d2a46f4afb943ca8328872961865ee3e
Braian <troncosobraianaxel@gmail.com>
2023-10-14 15:22:40 -0300

    [ADD] User api controllers ok prueba

--- backend/workflix/src/main/java/com/tec/workflix/controllers/UserApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/UserApiController.java
@@ -1,7 +1,14 @@
 package com.tec.workflix.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class UserApiController {
+    @GetMapping("/test")
+    public String testEndPoint(){
+        return "Test end point is working";
+    }
 }
