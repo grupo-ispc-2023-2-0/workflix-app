853ec10a16239cc0ddf5e9a4ca95306fae696c95
Braian <troncosobraianaxel@gmail.com>
2023-10-14 15:13:55 -0300

    [ADD] Creando script db - add datos

---
+++ backend/workflix/src/main/resources/db_script/main.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE workflix;
+USE workflix;
+-- USER TABLE STRUCTURE
+CREATE TABLE users(
+    user_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    PRIMARY KEY(user_id)
+);
