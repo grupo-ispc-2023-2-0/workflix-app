e3dbbad9a855f09e610edf9c28410daa40a79824
Braian <troncosobraianaxel@gmail.com>
2023-10-14 16:17:59 -0300

    [ADD] add act codigo

--- backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
@@ -4,11 +4,14 @@ import com.tec.workflix.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class RegisterApiController {
 
     @Autowired
@@ -23,6 +26,15 @@ public class RegisterApiController {
             return new ResponseEntity<>("Porfavor completa todas las celdas", HttpStatus.BAD_REQUEST);
         }
         // ENCRIPTACION / HASH PASSWORD
+        String hashed_password = BCrypt.hashpw(password, BCrypt.gensalt());
 
+        // REGISTER NEW USER
+        int result = userService.registerNewUserServiceMethod(first_name, last_name, email, hashed_password);
+
+        if (result !=1){
+            return new ResponseEntity<>("FALLO AL INTENTAR REGISTRARSE",HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>("USUARIO REGISTRADO CORRECTAMENTE",HttpStatus.OK);
     }
 }
