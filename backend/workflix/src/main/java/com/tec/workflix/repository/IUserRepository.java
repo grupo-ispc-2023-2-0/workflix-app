8ad0c0eddc8473aa53469821d12ef9e8d6b2f276
Braian <troncosobraianaxel@gmail.com>
2023-10-14 15:31:23 -0300

    [ADD] User repository Query

--- backend/workflix/src/main/java/com/tec/workflix/repository/IUserRepository.java
+++ backend/workflix/src/main/java/com/tec/workflix/repository/IUserRepository.java
@@ -1,10 +1,22 @@
 package com.tec.workflix.repository;
 
 import com.tec.workflix.models.Users;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface IUserRepository extends CrudRepository<Users,Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO USERS(first_name, last_name, email, password) VALUES(:first_name, :last_name, :email, :password)", nativeQuery = true)
+    int registerNewUser(@Param("first_name") String first_name,
+                        @Param("last_name") String last_name,
+                        @Param("email") String email,
+                        @Param("password") String password);
 }
