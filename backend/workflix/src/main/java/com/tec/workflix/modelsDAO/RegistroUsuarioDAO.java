82e3f4d3fcecfd1c903d3ed8f390a06614c7f13e
Braian <troncosobraianaxel@gmail.com>
2023-10-14 02:48:10 -0300

    [ADD] Registro Usuario DAO

---
+++ backend/workflix/src/main/java/com/tec/workflix/modelsDAO/RegistroUsuarioDAO.java
@@ -0,0 +1,16 @@
+package com.tec.workflix.modelsDAO;
+
+import com.tec.workflix.interfaces.IAutenticacionUsuario;
+import com.tec.workflix.models.Usuario;
+
+public class RegistroUsuarioDAO implements IAutenticacionUsuario {
+    @Override
+    public Usuario obtenerUsuario(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public Usuario registrarUsuario(Usuario usuario) {
+        return null;
+    }
+}
