00fab147419009802a5228db37b093d8ea29354a
Braian <troncosobraianaxel@gmail.com>
2023-10-14 15:40:56 -0300

    [ADD] Register User Logica

--- backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
@@ -1,7 +1,28 @@
 package com.tec.workflix.controllers;
 
+import com.tec.workflix.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterApiController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/user/register")
+    public ResponseEntity registerNewUser(@RequestParam("first_name") String first_name,
+                                          @RequestParam("last_name") String last_name,
+                                          @RequestParam("email") String email,
+                                          @RequestParam("password") String password){
+        if(first_name.isEmpty() || last_name.isEmpty() || email.isEmpty() || password.isEmpty()){
+            return new ResponseEntity<>("Porfavor completa todas las celdas", HttpStatus.BAD_REQUEST);
+        }
+        // ENCRIPTACION / HASH PASSWORD
+
+    }
 }
--- backend/workflix/src/main/java/com/tec/workflix/services/UserService.java
+++ backend/workflix/src/main/java/com/tec/workflix/services/UserService.java
@@ -1,7 +1,15 @@
 package com.tec.workflix.services;
 
+import com.tec.workflix.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    IUserRepository userRepository;
+
+    public int registerNewUserServiceMethod(String fname, String lname, String email, String password){
+        return userRepository.registerNewUser(fname,lname,email,password);
+    }
 }
