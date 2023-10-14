b2febeb057a142c61ba55a6110b724bf2a3fec34
Braian <troncosobraianaxel@gmail.com>
2023-10-14 14:52:42 -0300

    [DEL] archivos no necesarios

--- backend/workflix/src/main/java/com/tec/workflix/interfaces/IAutenticacionUsuario.java
+++
@@ -1,8 +0,0 @@
-package com.tec.workflix.interfaces;
-
-import com.tec.workflix.models.Usuario;
-
-public interface IAutenticacionUsuario {
-    public boolean autenticarUsuario(String correo, String clave);
-    public Usuario registrarUsuario(Usuario usuario);
-}
