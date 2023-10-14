568da264804f5e6d11a6b2b6cdf35ba21bbf4016
Braian <troncosobraianaxel@gmail.com>
2023-10-14 02:57:19 -0300

    [ADD] Registro Usuario DAO

--- backend/workflix/src/main/java/com/tec/workflix/modelsDAO/RegistroUsuarioDAO.java
+++ backend/workflix/src/main/java/com/tec/workflix/modelsDAO/RegistroUsuarioDAO.java
@@ -2,15 +2,28 @@ package com.tec.workflix.modelsDAO;
 
 import com.tec.workflix.interfaces.IAutenticacionUsuario;
 import com.tec.workflix.models.Usuario;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class RegistroUsuarioDAO implements IAutenticacionUsuario {
+
+    JdbcTemplate template;
     @Override
     public Usuario obtenerUsuario(Usuario usuario) {
-        return null;
+        String sql = "SELECT * FROM usuario WHERE clave = ? AND correo = ?";
+        try {
+            return template.queryForObject(sql, new Object[]{usuario.getClave(), usuario.getCorreo()}, new UsuarioMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public Usuario registrarUsuario(Usuario usuario) {
-        return null;
+        String sql = "INSERT INTO usuario(nombre, apellido, clave, telefono, correo) VALUES (?, ?, ?, ?, ?)";
+        template.update(sql, usuario.getNombre(), usuario.getApellido(), usuario.getClave(), usuario.getTelefono(), usuario.getCorreo());
+        return usuario;
     }
 }
