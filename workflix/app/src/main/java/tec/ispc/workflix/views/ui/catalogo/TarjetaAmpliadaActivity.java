b7c8522337877ba96ffcfadeb7e2adbe450a255f
marceloedelgado <marceloedelgado@gmail.com>
2023-10-15 20:57:25 -0300

    se agrega tarjeta ampliada del profesional

---
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/catalogo/TarjetaAmpliadaActivity.java
@@ -0,0 +1,16 @@
+package tec.ispc.workflix.views.ui.catalogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tec.ispc.workflix.R;
+
+public class TarjetaAmpliadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarjeta_ampliada);
+    }
+}
