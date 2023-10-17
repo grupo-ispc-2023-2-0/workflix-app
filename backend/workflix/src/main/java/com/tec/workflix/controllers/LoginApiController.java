b6802140954794bb371fd46ff927dc00d0db3871
Braian <troncosobraianaxel@gmail.com>
2023-10-17 00:42:59 -0300

    [ADD] loginApiControllers def post

--- backend/workflix/src/main/java/com/tec/workflix/controllers/LoginApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/LoginApiController.java
@@ -1,7 +1,13 @@
 package com.tec.workflix.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginApiController {
+    @PostMapping("/user/login")
+    public ResponseEntity authenticateUser(){
+        return new ResponseEntity();
+    }
 }
