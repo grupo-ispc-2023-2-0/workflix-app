05b725c6a387b013c37ae51d381954032713c322
Braian <troncosobraianaxel@gmail.com>
2023-10-17 01:25:34 -0300

    [ADD] todo ok sin errores

--- backend/workflix/src/main/java/com/tec/workflix/controllers/LoginApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/LoginApiController.java
@@ -1,17 +1,21 @@
 package com.tec.workflix.controllers;
 
 import com.tec.workflix.models.Login;
+import com.tec.workflix.models.Users;
 import com.tec.workflix.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class LoginApiController {
     @Autowired
     UserService userService;
@@ -29,6 +33,16 @@ public class LoginApiController {
         // End of check if emails is empty
 
 
-        return new ResponseEntity();
+        // Get Hashed Users Password
+        String hashed_password = userService.checkUserPasswordByEmail(login.getEmail());
+
+        // Validate User Password
+        if (!BCrypt.checkpw(login.getPassword(),hashed_password)){
+            return new ResponseEntity("Incorrecto Username o password",HttpStatus.BAD_REQUEST);
+        }
+
+        // Set User Object
+        Users user = userService.getUserDetailByEmail(login.getEmail());
+        return new ResponseEntity(user, HttpStatus.OK);
     }
 }
