38feff409f430eb2d61f9a42a3f6c796ff0a5601
Braian <troncosobraianaxel@gmail.com>
2023-10-17 01:06:03 -0300

    [ADD] User controllers inyect to dependency from user services

--- backend/workflix/src/main/java/com/tec/workflix/controllers/LoginApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/LoginApiController.java
@@ -1,13 +1,34 @@
 package com.tec.workflix.controllers;
 
+import com.tec.workflix.models.Login;
+import com.tec.workflix.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoginApiController {
+    @Autowired
+    UserService userService;
+
     @PostMapping("/user/login")
-    public ResponseEntity authenticateUser(){
+    public ResponseEntity authenticateUser(@RequestBody Login login){
+
+        // Get user email:
+        List<String> userEmail = userService.checkUserEmail(login.getEmail());
+
+        // Check IF email is Empty
+        if(userEmail.isEmpty() || userEmail == null){
+            return new ResponseEntity("El correo no existe", HttpStatus.NOT_FOUND);
+        }
+        // End of check if emails is empty
+
+
         return new ResponseEntity();
     }
 }
