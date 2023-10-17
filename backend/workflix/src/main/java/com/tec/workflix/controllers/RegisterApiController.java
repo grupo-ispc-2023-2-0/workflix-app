db9109a9ef8714b1eb4d868e4915174f9412b94a
Braian <troncosobraianaxel@gmail.com>
2023-10-16 22:27:43 -0300

    [ADD] inyeccion dependencias logback - slf4j

--- backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
@@ -1,6 +1,8 @@
 package com.tec.workflix.controllers;
 
 import com.tec.workflix.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class RegisterApiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RegisterApiController.class);
 
     @Autowired
     UserService userService;
