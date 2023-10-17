61fe134dbbbc977e2695ed88ed8a4f9ae15e2f53
Braian <troncosobraianaxel@gmail.com>
2023-10-16 22:23:28 -0300

    [ADD] logback

--- backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/RegisterApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 public class RegisterApiController {
 
     @Autowired
--- backend/workflix/src/main/java/com/tec/workflix/repository/IUserRepository.java
+++ backend/workflix/src/main/java/com/tec/workflix/repository/IUserRepository.java
@@ -1,13 +1,13 @@
 package com.tec.workflix.repository;
 
 import com.tec.workflix.models.Users;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface IUserRepository extends CrudRepository<Users,Integer> {
