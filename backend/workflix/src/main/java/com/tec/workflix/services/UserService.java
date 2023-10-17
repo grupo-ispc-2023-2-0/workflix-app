8621f7e7a48dbc609d82e02a8e5471075f7cd405
Braian <troncosobraianaxel@gmail.com>
2023-10-17 00:53:12 -0300

    [ADD] User service method checkUserPasswordByEmail

--- backend/workflix/src/main/java/com/tec/workflix/services/UserService.java
+++ backend/workflix/src/main/java/com/tec/workflix/services/UserService.java
@@ -4,6 +4,8 @@ import com.tec.workflix.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -11,5 +13,9 @@ public class UserService {
 
     public int registerNewUserServiceMethod(String fname, String lname, String email, String password){
         return userRepository.registerNewUser(fname,lname,email,password);
-    }
+    }// Fin registro de usuario metodo
+
+    public List<String> checkUserPasswordByEmail(String email){
+        return userRepository.checkUserPasswordByEmail(email);
+    }// Fin metodo para checkear password por email
 }
