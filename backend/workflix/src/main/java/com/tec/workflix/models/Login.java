4b9f1716b194fec6d22677af3d4a2d5c95083c73
Braian <troncosobraianaxel@gmail.com>
2023-10-19 04:28:35 -0300

    [REFACTOR] quitando código de mas

--- backend/workflix/src/main/java/com/tec/workflix/models/Login.java
+++ backend/workflix/src/main/java/com/tec/workflix/models/Login.java
@@ -7,19 +7,4 @@ public class Login {
     private String correo;
     private String clave;
 
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
 }
--- backend/workflix/src/main/java/com/tec/workflix/models/Profesional.java
+++ backend/workflix/src/main/java/com/tec/workflix/models/Profesional.java
@@ -7,6 +7,12 @@ import java.util.List;
 import java.util.Map;
 @Data
 public class Profesional extends Usuario{
-    private String telefono, direccion, correo, ciudad, provincia, descripcion, foto, valoracion, profesion;
-
+    private String telefono;
+    private String direccion;
+    private String correo;
+    private String ciudad;
+    private String provincia;
+    private String descripcion;
+    private String foto;
+    private String profesion;
 }
