fad80950e74206e7273c757f2866200eeeae5cb3
Erick <105466809+erick-lpz@users.noreply.github.com>
2023-10-20 20:28:59 -0300

    Se creo la activity de usuarios en el dashboard

---
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/dashboard_admin/DashboardUsuarios.java
@@ -0,0 +1,16 @@
+package tec.ispc.workflix.views.ui.dashboard_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tec.ispc.workflix.R;
+
+public class DashboardUsuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_usuarios);
+    }
+}
