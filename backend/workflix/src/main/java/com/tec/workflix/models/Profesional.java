6df2accad52f5723323f812995d5199e8b7d8339
Braian <troncosobraianaxel@gmail.com>
2023-10-19 21:04:25 -0300

    [ADD] creando modelo profesional

--- backend/workflix/src/main/java/com/tec/workflix/models/Profesional.java
+++ backend/workflix/src/main/java/com/tec/workflix/models/Profesional.java
@@ -1,18 +1,28 @@
 package com.tec.workflix.models;
 
 
+import jakarta.persistence.Column;
 import lombok.Data;
 
 import java.util.List;
 import java.util.Map;
 @Data
+
 public class Profesional extends Usuario{
+    @Column
     private String telefono;
+    @Column
     private String direccion;
+    @Column
     private String correo;
+    @Column
     private String ciudad;
+    @Column
     private String provincia;
+    @Column
     private String descripcion;
+    @Column
     private String foto;
+    @Column
     private String profesion;
 }
