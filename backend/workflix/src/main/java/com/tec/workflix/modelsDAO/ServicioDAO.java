7dd07abd855813aaeada74b9e84aa09a3ab0fd69
Braian <troncosobraianaxel@gmail.com>
2023-10-21 00:44:36 -0300

    [ADD] Servicio DAO - CRUD - Completo

--- backend/workflix/src/main/java/com/tec/workflix/modelsDAO/ServicioDAO.java
+++ backend/workflix/src/main/java/com/tec/workflix/modelsDAO/ServicioDAO.java
@@ -23,21 +23,25 @@ public class ServicioDAO implements IServicioInterface {
 
     @Override
     public List<Map<String, Object>> listarId(int id) {
-        return null;
+        List<Map<String, Object>> list = template.queryForList("select * from servicio");
+        return list;
     }
 
     @Override
     public int add(Servicio servicio) {
-        return 0;
+        String sql = "insert into servicio(nombre)values(?)";
+        return template.update(sql, servicio.getNombre());
     }
 
     @Override
     public int edit(Servicio servicio) {
-        return 0;
+        String sql="update servicio set nombre=? where id=?";
+        return template.update(sql,servicio.getNombre(),servicio.getId());
     }
 
     @Override
     public int delete(int id) {
-        return 0;
+        String sql="delete from servicio where id=?";
+        return template.update(sql,id);
     }
 }
