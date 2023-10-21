6289eab5a034139372d4ef8e61edb21eb255d4a0
Braian <troncosobraianaxel@gmail.com>
2023-10-21 01:25:19 -0300

    [ADD] Servicio service - implements - api

--- workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
+++ workflix/app/src/main/java/tec/ispc/workflix/utils/Apis.java
@@ -4,10 +4,13 @@ import retrofit2.Call;
 import tec.ispc.workflix.models.Usuario;
 
 public class Apis {
-    //public static final String URL_001="http://192.168.0.125:8080/personas/";
+    public static final String URL_001="http://192.168.0.125:8080/servicios/";
     public static final String URL_002="http://192.168.0.125:8080/usuarios/";
 
 
+    public static ServicioService getServicioService(){
+        return Cliente.getCliente(URL_001).create(ServicioService.class);
+    }
     public static UsuarioService getUsuarioService(){
         return Cliente.getCliente(URL_002).create(UsuarioService.class);
     }
