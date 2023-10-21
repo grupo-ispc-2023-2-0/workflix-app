9f1762cdbd968f036c346bd261c5eb6ea2947ef3
Braian <troncosobraianaxel@gmail.com>
2023-10-21 01:20:35 -0300

    [ADD] model servicio - cliente - serializer - construct - gtters - setters

--- workflix/app/src/main/java/tec/ispc/workflix/models/Servicio.java
+++ workflix/app/src/main/java/tec/ispc/workflix/models/Servicio.java
@@ -12,4 +12,27 @@ public class Servicio {
     @Expose
     private String nombre;
 
+    public Servicio() {
+    }
+
+    public Servicio(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
