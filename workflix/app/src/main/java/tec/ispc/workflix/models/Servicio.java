a94d3c1ee0583235d129e5f05a10bc4026fd841b
Braian <troncosobraianaxel@gmail.com>
2023-10-21 01:19:49 -0300

    [ADD] model servicio - cliente

--- workflix/app/src/main/java/tec/ispc/workflix/models/Servicio.java
+++ workflix/app/src/main/java/tec/ispc/workflix/models/Servicio.java
@@ -1,4 +1,15 @@
 package tec.ispc.workflix.models;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Servicio {
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("nombre")
+    @Expose
+    private String nombre;
+
 }
