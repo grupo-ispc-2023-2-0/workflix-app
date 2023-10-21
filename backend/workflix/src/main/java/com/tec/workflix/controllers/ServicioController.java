b24eb8118be87cdba1acd9a41f5246d1013980e5
Braian <troncosobraianaxel@gmail.com>
2023-10-21 00:48:46 -0300

    [ADD] servicio controller - mapping

--- backend/workflix/src/main/java/com/tec/workflix/controllers/ServicioController.java
+++ backend/workflix/src/main/java/com/tec/workflix/controllers/ServicioController.java
@@ -1,4 +1,10 @@
 package com.tec.workflix.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/servicios")
 public class ServicioController {
+
 }
