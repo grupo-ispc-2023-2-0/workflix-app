f02f37acb1b54f3e7065caea0f791938f151020d
dfg259 <105681436+dfg259@users.noreply.github.com>
2023-10-22 15:32:18 -0300

    se agrega icono

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/guardar_datos/guardar_datos.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/guardar_datos/guardar_datos.java
@@ -2,9 +2,12 @@ package tec.ispc.workflix.views.ui.guardar_datos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import tec.ispc.workflix.R;
+import tec.ispc.workflix.views.MainActivity;
 
 public class guardar_datos extends AppCompatActivity {
 
@@ -13,4 +16,10 @@ public class guardar_datos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guardar_datos);
     }
+
+    public void btEnviar2 (View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
