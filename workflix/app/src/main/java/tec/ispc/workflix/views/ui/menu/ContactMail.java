214b492460f66dba8533015ad225d4ad90c3ac59
dfg259 <105681436+dfg259@users.noreply.github.com>
2023-10-24 18:56:53 -0300

    se da funcionalidad a boton enviar en formulario de contacto

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/ContactMail.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/ContactMail.java
@@ -1,5 +1,6 @@
 package tec.ispc.workflix.views.ui.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import tec.ispc.workflix.R;
+import tec.ispc.workflix.views.MainActivity;
+import tec.ispc.workflix.views.ui.perfil.Perfil;
 
 
 public class ContactMail extends Fragment {
+    private Button btEnviar;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_mail, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact_mail, container, false);
+
+        btEnviar = view.findViewById(R.id.btEnviar);
+        btEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
