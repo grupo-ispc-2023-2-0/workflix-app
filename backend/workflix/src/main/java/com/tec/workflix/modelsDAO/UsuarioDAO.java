a12d2f92918539896382b98919b48c0575266a52
Braian <troncosobraianaxel@gmail.com>
2023-10-26 03:23:14 -0300

    [ADD] implementando actPerfil interface

--- backend/workflix/src/main/java/com/tec/workflix/modelsDAO/UsuarioDAO.java
+++ backend/workflix/src/main/java/com/tec/workflix/modelsDAO/UsuarioDAO.java
@@ -40,7 +40,7 @@ public class UsuarioDAO implements IUsuarioInterface {
     }
     @Override
     public int actPerfil(Usuario usuario){
-        String sql="update usuario set nombre=?, apellido=?, telefono=?, correo=?, ciudad=?, provincia=?, profesion=?, descripcion=?";
+        String sql="update usuario set nombre=?, apellido=?, telefono=?, correo=?, ciudad=?, provincia=?, profesion=?, descripcion=? where id=?";
     return template.update(sql,usuario.getNombre(),usuario.getApellido(),usuario.getTelefono(),usuario.getCorreo(),usuario.getCiudad(), usuario.getProvincia()
     ,usuario.getProfesion(),usuario.getDescripcion());
     }
