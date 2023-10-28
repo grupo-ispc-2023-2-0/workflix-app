bb0ca362dc73fa6ce930866d60745eadc9a4864e
Braian <troncosobraianaxel@gmail.com>
2023-10-28 00:49:43 -0300

    [ADD] recuperando foto del cel - steando put - preferences

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/login/LoginActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/login/LoginActivity.java
@@ -105,9 +105,8 @@ public class LoginActivity extends AppCompatActivity {
                             String descripcion = response.isNull("descripcion") ? "" : response.getString("descripcion");
                             String provincia = response.isNull("provincia") ? "" : response.getString("provincia");
                             String profesion = response.isNull("profesion") ? "" : response.getString("profesion");
+                            String foto = response.isNull("foto") ? "" : response.getString("foto");
 
-                            /*String foto = (String) response.get("foto");
-                            */
                             boolean is_admin = response.getBoolean("is_admin");
 
                             // Guardar los datos del usuario en SharedPreferences
@@ -122,7 +121,7 @@ public class LoginActivity extends AppCompatActivity {
                             editor.putString("descripcion",descripcion);
                             editor.putString("provincia",provincia);
                             editor.putString("profesion",profesion);
-                           /* editor.putString("foto",foto); */
+                            editor.putString("foto",foto);
                             editor.putBoolean("is_admin",is_admin);
                             editor.apply();
                             // Redirigir al usuario a MainActivity
