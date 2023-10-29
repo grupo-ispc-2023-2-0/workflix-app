8bf0be97e48dbb965606863c3a25db023a2431ef
Braian <troncosobraianaxel@gmail.com>
2023-10-29 14:04:58 -0300

    [ADD] cod act

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/catalogo/CatalogoAdapter.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/catalogo/CatalogoAdapter.java
@@ -1,5 +1,7 @@
 package tec.ispc.workflix.views.ui.catalogo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +18,11 @@ import tec.ispc.workflix.models.Usuario;
 
 public class CatalogoAdapter extends RecyclerView.Adapter<CatalogoAdapter.CatalogoViewHolder> {
     private List<Usuario> listaDeUsuarios;
+    private Context context;
 
-    public CatalogoAdapter(List<Usuario> listaDeUsuarios) {
+    public CatalogoAdapter(List<Usuario> listaDeUsuarios, Context context) {
         this.listaDeUsuarios = listaDeUsuarios;
+        this.context = context;
     }
 
     public void setUsuarios(List<Usuario> usuarios) {
@@ -33,7 +37,7 @@ public class CatalogoAdapter extends RecyclerView.Adapter<CatalogoAdapter.Catalo
     }
 
     @Override
-    public void onBindViewHolder(CatalogoViewHolder holder, int position) {
+    public void onBindViewHolder(CatalogoViewHolder holder, final int position) {
         Usuario usuario = listaDeUsuarios.get(position);
 
         String nombreCompleto = usuario.getNombre() + " " + usuario.getApellido();
@@ -41,6 +45,13 @@ public class CatalogoAdapter extends RecyclerView.Adapter<CatalogoAdapter.Catalo
         holder.perfilNombre.setText(nombreCompleto);
         holder.perfilDescripcion.setText(usuario.getDescripcion());
         Picasso.get().load(usuario.getFoto()).into(holder.imagenFoto);
+
+        holder.botonConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirTarjetaAmpliadaActivity(position);
+            }
+        });
     }
 
     @Override
@@ -53,6 +64,7 @@ public class CatalogoAdapter extends RecyclerView.Adapter<CatalogoAdapter.Catalo
         public TextView perfilNombre;
         public TextView perfilDescripcion;
         public ImageView imagenFoto;
+        public Button botonConsultar;
 
         public CatalogoViewHolder(View itemView) {
             super(itemView);
@@ -60,6 +72,27 @@ public class CatalogoAdapter extends RecyclerView.Adapter<CatalogoAdapter.Catalo
             perfilNombre = itemView.findViewById(R.id.perfilNombre);
             perfilDescripcion = itemView.findViewById(R.id.perfilDescripcion);
             imagenFoto = itemView.findViewById(R.id.imagenFoto);
+            botonConsultar = itemView.findViewById(R.id.botonConsultar);
         }
     }
+
+    private void abrirTarjetaAmpliadaActivity(int position) {
+        Usuario usuario = listaDeUsuarios.get(position);
+        Intent intent = new Intent(context, TarjetaAmpliadaActivity.class);
+
+        // Pasa los datos individuales del usuario a través del Intent
+        intent.putExtra("nombreCompleto", usuario.getNombre() + " " + usuario.getApellido());
+        intent.putExtra("imagenURL", usuario.getFoto());
+        intent.putExtra("descripcion", usuario.getDescripcion());
+        intent.putExtra("correo", usuario.getCorreo());
+        intent.putExtra("telefono", usuario.getTelefono());
+        intent.putExtra("ciudad", usuario.getCiudad());
+        intent.putExtra("provincia", usuario.getProvincia());
+        // Agrega más extras según sea necesario
+
+        context.startActivity(intent);
+    }
+
+
+
 }
--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/catalogo/TarjetaAmpliadaActivity.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/catalogo/TarjetaAmpliadaActivity.java
@@ -1,9 +1,10 @@
 package tec.ispc.workflix.views.ui.catalogo;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import com.squareup.picasso.Picasso;
 import tec.ispc.workflix.R;
 
 public class TarjetaAmpliadaActivity extends AppCompatActivity {
@@ -13,14 +14,60 @@ public class TarjetaAmpliadaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarjeta_ampliada);
 
+        // Obtén los datos pasados desde CatalogoAdapter
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            int profesionalPosicion = extras.getInt("profesional_posicion", -1);
+            String nombreCompleto = extras.getString("nombreCompleto");
+            String apellido = extras.getString("apellido");
+            String imagenURL = extras.getString("imagenURL");
+            String servicio = extras.getString("servicio");
+            String descripcion = extras.getString("descripcion");
+            String correo = extras.getString("correo");
+            String telefono = extras.getString("telefono");
+            String ciudad = extras.getString("ciudad");
+            String provincia = extras.getString("provincia");
 
-            if (profesionalPosicion != -1) {
+            // Ahora puedes mostrar estos datos en tus TextViews
+            TextView perfilNombreTextView = findViewById(R.id.perfilNombre);
+            TextView perfilApellidoTextView = findViewById(R.id.perfilApellido);
+            ImageView imagenFotoImageView = findViewById(R.id.imagenFoto);
+            TextView perfilServicioTextView = findViewById(R.id.perfilServicio);
+            TextView perfilDescripcionTextView = findViewById(R.id.perfilDescripcion);
+            TextView perfilCorreoTextView = findViewById(R.id.perfilCorreo);
+            TextView perfilTelefonoTextView = findViewById(R.id.perfilTelefono);
+            TextView perfilCiudadTextView = findViewById(R.id.perfilCiudad);
+            TextView perfilProvinciaTextView = findViewById(R.id.perfilProvincia);
 
-                TextView textView = findViewById(R.id.textView);
-                textView.setText("Mostrando detalles del profesional en la posición: " + profesionalPosicion);
+
+            if (nombreCompleto != null) {
+                perfilNombreTextView.setText(nombreCompleto);
+            }
+            if (apellido != null) {
+                perfilApellidoTextView.setText(apellido);
+            }
+
+            if (imagenURL != null) {
+
+                Picasso.get().load(imagenURL).into(imagenFotoImageView);
+            }
+
+            if (servicio != null) {
+                perfilServicioTextView.setText(servicio);
+            }
+            if (descripcion != null) {
+                perfilDescripcionTextView.setText(descripcion);
+            }
+            if (correo != null) {
+                perfilCorreoTextView.setText(correo);
+            }
+            if (telefono != null) {
+                perfilTelefonoTextView.setText(telefono);
+            }
+            if (ciudad != null) {
+                perfilCiudadTextView.setText(ciudad);
+            }
+            if (provincia != null) {
+                perfilProvinciaTextView.setText(provincia);
             }
         }
     }
