9e6e4a3a732442e318ac9fed91936597caf1af74
Braian <troncosobraianaxel@gmail.com>
2023-11-04 23:55:08 -0300

    [ADD] ocultando btn login - perfil adm and users prof

--- workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/HomeFragment.java
+++ workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/HomeFragment.java
@@ -1,6 +1,8 @@
 package tec.ispc.workflix.views.ui.menu;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.android.material.navigation.NavigationView;
 
 import tec.ispc.workflix.R;
 import tec.ispc.workflix.views.ui.catalogo.CatalogoActivity;
@@ -21,6 +25,8 @@ public class HomeFragment extends Fragment {
     private Button btn_home2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+/*        mostrarElementosHome();*/
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
@@ -34,9 +40,6 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
-
-
         btn_home = view.findViewById(R.id.btn_home);
         btn_home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,8 +49,16 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
-
         return view;
 
+    }
+    public void mostrarElementosHome() {
+        SharedPreferences preferences = getActivity().getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        if (preferences.contains("nombre")) {
+            btn_home.setVisibility(View.VISIBLE);
+        } else {
+            btn_home.setVisibility(View.GONE);
+
+        }
     }
 }
